3bcc6ff19ccb0d4afc1322db208804f14965d553
Kaique Bosco <kaique.bosco@hotmail.com>
2023-07-22 19:03:34 -0300

    add arquivos exercicio gulp

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const sourcemaps = require("gulp-sourcemaps");
+const uglify = require("gulp-uglify");
+const obfuscate = require("gulp-obfuscate");
+const imagemin = require("gulp-imagemin");
+
+function compileSass() {
+    return gulp
+        .src("./source/styles/main.scss")
+        .pipe(sourcemaps.init())
+        .pipe(
+            sass({
+                outputStyle: "compressed",
+            })
+        )
+        .pipe(sourcemaps.write("./maps"))
+        .pipe(gulp.dest("./build/styles"));
+}
+
+function compressesImages() {
+    return gulp
+        .src("./source/images/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("./build/images"));
+}
+
+function compressesJS() {
+    return gulp
+        .src("./source/scripts/*.js")
+        .pipe(uglify())
+        .pipe(obfuscate())
+        .pipe(gulp.dest("./build/scripts"));
+}
+
+exports.default = function () {
+    gulp.watch(
+        "./source/styles/*.scss",
+        { ignoreInitial: false },
+        gulp.series(compileSass)
+    );
+    gulp.watch(
+        "./source/scripts/*.js",
+        { ignoreInitial: false },
+        gulp.series(compressesJS)
+    );
+    gulp.watch(
+        "./source/images/*",
+        { ignoreInitial: false },
+        gulp.series(compressesImages)
+    );
+};
