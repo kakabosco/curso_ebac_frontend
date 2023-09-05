754c9a4a0dedf13963a0e2638aa2010a99a6ab98
Kaique Bosco <kaique.bosco@hotmail.com>
2023-09-05 12:46:13 -0300

    add ts exercise

---
+++ src/functions.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiply(a, b) {
+    return a * b;
+}
+function greet(name) {
+    return "Olá " + name;
+}
+console.log(multiply(5, 7));
+console.log(greet("Kaique"));
---
+++ src/functions.ts
@@ -0,0 +1,10 @@
+function multiply(a: number, b: number): number {
+    return a * b;
+}
+
+function greet(name: string): string {
+    return "Olá " + name;
+}
+
+console.log(multiply(5, 7));
+console.log(greet("Kaique"));
